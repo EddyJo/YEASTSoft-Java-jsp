de24e43ab718792a860d679cc635f9b7ec35fd76
EddyJo <jkjk0600@gmail.com>
2016-09-05 00:29:54 +0900

    약과 음식정보 모아서 DB에 저장하는 기능
    틀만 잡았고, 
    모델에서 크롤링하는 기능도 필요할 것 같다.
    DAO로 다시 만들어야 한다.
    그리고 입력창 또한 필요하다.

---
+++ WebMVCProjectTemplate/src/kr/co/javaspecialist/collect/controller/CollectController.java
@@ -0,0 +1,22 @@
+package kr.co.javaspecialist.collect.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.javaspecialist.collect.service.CollectService;
+import kr.co.javaspecialist.common.controller.CommandHandler;
+
+public class CollectController implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) {
+		String medi = request.getParameter("medi");
+		String url = request.getParameter("url");
+		CollectService collector = new CollectService();
+		String result = collector.collect(url, medi);
+		request.setAttribute("result", result);
+		return "/collect/collect.jsp";
+		//여기서든 DB에 저장되는 모델이므로 forward가 아니라 redirect 이용
+	}
+
+}
---
+++ WebMVCProjectTemplate/src/kr/co/javaspecialist/collect/service/CollectService.java
@@ -0,0 +1,8 @@
+package kr.co.javaspecialist.collect.service;
+
+public class CollectService {
+	public String collect(String url, String medi){
+		return medi;
+		//여기서는 DAO를 이용해서 DB에 저장되게끔 구현해야 한다.
+	}
+}
