5a74bfadc14d2f538c951c2e935737d063e63de5
yejiyaji <diwldidy@naver.com>
2016-09-09 13:26:41 +0900

    member DB 완성 search_log DB 하는 중 - 약품DB 만들어지면 완성됨

--- WebMVCProjectTemplate/WebContent/WEB-INF/classes/db/oracle/member_oracle.sql
+++ WebMVCProjectTemplate/WebContent/WEB-INF/classes/db/oracle/member_oracle.sql
@@ -10,9 +10,18 @@ CREATE TABLE MEMBER(
 ALTER TABLE MEMBER ADD CONSTRAINT IDX_MEMBER_PK PRIMARY KEY (USERID);
 
 INSERT INTO MEMBER (USERID, NAME, PASSWORD, PHONE, EMAIL)
-VALUES ('admin', 'JinKyoung Heo', '1234', '010-3402-7902', 'admin@medicine.com');
+VALUES ('admin', '관리자', '1234', '010-3402-7902', 'admin@medicine.com');
 
 INSERT INTO MEMBER (USERID, NAME, PASSWORD, PHONE, EMAIL)
-VALUES ('user1', '홍길동', 'user1234', '010-1234-5678', 'kildong@hong.com');
+VALUES ('dachan', '황다찬', '1111', '010-1234-1111', 'chan@gmail.com');
+
+INSERT INTO MEMBER (USERID, NAME, PASSWORD, PHONE, EMAIL)
+VALUES ('yeji', '하예지', '2222', '010-1234-2222', 'yeji@gmail.com');
+
+INSERT INTO MEMBER (USERID, NAME, PASSWORD, PHONE, EMAIL)
+VALUES ('jiin', '박지인', '3333', '010-1234-3333', 'parkJI@gmail.com');
+
+INSERT INTO MEMBER (USERID, NAME, PASSWORD, PHONE, EMAIL)
+VALUES ('kyung', '김경은', '4444', '010-1234-4444', 'kyungEunKim@gmail.com');
 
 commit;
